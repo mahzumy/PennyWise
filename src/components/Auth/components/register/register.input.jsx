861a57f1d1882dd16da0d32e9866e184bbee0752
Nuha Fadh <fayn.deco@gmail.com>
2023-10-16 18:30:00 +0700

    add re-type password

--- src/components/Auth/components/register/register.input.jsx
+++ src/components/Auth/components/register/register.input.jsx
@@ -20,6 +20,10 @@ export const RegisterInput = () => {
                 <label>Password</label>
                 <input type='password' name='password' placeholder='password' onChange={handleChange}/>
               </div>          
+              <div>
+                <label>Re-type Password</label>
+                <input type='password' name='password' placeholder='password' onChange={handleChange}/>
+              </div>          
             </div>
             <button className='w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800' onClick={handleSubmitRegister}>Register</button>
         </div>
