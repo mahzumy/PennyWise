b6ff3b780d12a52b813999d2ec31a7a2e4ccfeb5
Nuha Fadh <fayn.deco@gmail.com>
2023-10-22 03:02:48 +0700

    placeholder graph tapi masih pakai static data. Belum dinamis

--- src/components/dashboard/dashboard.jsx
+++ src/components/dashboard/dashboard.jsx
@@ -2,6 +2,75 @@
 import { useRouter } from 'next/navigation';
 import React from 'react'
 import Image from 'next/image';
+import {
+  AreaChart,
+  Card,
+  Metric,
+  TabList,
+  Tab,
+  TabGroup,
+  TabPanels,
+  TabPanel,
+} from "@tremor/react";
+
+const data = [
+  {
+    Month: "Jan 17",
+    Spending: 75000,
+  },
+  //...
+  {
+    Month: "Jan 18",
+    Spending: 105000,
+  },
+  {
+    Month: "Jan 19",
+    Spending: 45000,
+  },
+  {
+    Month: "Jan 20",
+    Spending: 95000,
+  },
+  {
+    Month: "Jan 21",
+    Spending: 205000,
+  },
+  {
+    Month: "Jan 22",
+    Spending: 175000,
+  },
+  {
+    Month: "Jan 23",
+    Spending: 130000,
+  },
+  {
+    Month: "Jan 24",
+    Spending: 65000,
+  },
+  {
+    Month: "Jan 25",
+    Spending: 45000,
+  },
+  {
+    Month: "Jan 26",
+    Spending: 90000,
+  },
+  {
+    Month: "Jan 27",
+    Spending: 40000,
+  },
+];
+
+const numberFormatter = (value) => Intl.NumberFormat("us").format(value).toString();
+
+const percentageFormatter = (value) =>
+  `${Intl.NumberFormat("us")
+    .format(value * 100)
+    .toString()}%`;
+    
+function sumArray(array, metric) {
+  return array.reduce((accumulator, currentValue) => accumulator + currentValue[metric], 0);
+}
 
 export const Dashboard = ({transactionData}) => {
     const router = useRouter();
@@ -30,7 +99,8 @@ export const Dashboard = ({transactionData}) => {
         minimumFractionDigits:0,
       }).format(amount)
       return numAmount;
-    } 
+    };
+
 
     
   return (
@@ -96,6 +166,34 @@ export const Dashboard = ({transactionData}) => {
             })}
           </div>
         </div>
+        <div className='w-full p-6 bg-white border border-gray-200 rounded-lg shadow-lg dark:bg-gray-800 dark:border-gray-700'>
+        <Card className="p-0">
+            <TabGroup>
+              <TabList>
+                <Tab className="p-4 sm:p-6 text-left">
+                  <p className="text-md sm:text-base  text-gray-900 dark:text-white">Spending</p>
+                  <Metric className="mt-2 text-inherit text-red-500 dark:text-red-400">
+                    {numberFormatter(sumArray(data, "Spending"))}
+                  </Metric>
+                </Tab>
+              </TabList>
+              <TabPanels>
+                <TabPanel className="p-6">
+                  <AreaChart
+                    className="h-80 mt-10"
+                    data={data}
+                    index="Month"
+                    categories={["Spending"]}
+                    colors={["red"]}
+                    valueFormatter={numberFormatter}
+                    showLegend={false}
+                    yAxisWidth={50}
+                  />
+                </TabPanel>
+              </TabPanels>
+            </TabGroup>
+          </Card>
+          </div>
       </div>
     </div>
   )
