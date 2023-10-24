32baa4824092769a43fe541fb3e30d0120f9e40c
Nuha Fadh <fayn.deco@gmail.com>
2023-10-24 11:47:13 +0700

    adding back button

--- src/components/dashboard/all.transactions/all.transactions.jsx
+++ src/components/dashboard/all.transactions/all.transactions.jsx
@@ -33,6 +33,13 @@ export const Alltransactions = ({transactionData}) => {
      <div className='w-[450px] justify-center items-center m-auto'>
       <DashboardHeader />
       <div className='w-full p-6 bg-white border border-gray-200 rounded-lg shadow-lg dark:bg-gray-800 dark:border-gray-700 mb-4 space-y-5'>
+        <div className='flex space-x-1 cursor-pointer' onClick={()=>router.push(`/dashboard`)}>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-7 h-7 stroke-blue-600">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+          </svg>
+            <div className=' text-lg text-blue-600'>Back</div>
+        </div>
+        <hr />
         <div className='mb-2 text-2xl font-semibold tracking-tight  text-gray-900 dark:text-white'>All Transactions</div>
         <div className='flex justify-between my-2 items-center mx-2'>
           <div className='grid grid-cols-3 gap-2'>
