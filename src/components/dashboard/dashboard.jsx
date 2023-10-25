a1040b8647c5560fbfc54820d1b17c66bd0a8b88
Nuha Fadh <fayn.deco@gmail.com>
2023-10-26 05:30:14 +0700

    fix conflict graph merge - baru placeholder belum data real

--- src/components/dashboard/dashboard.jsx
+++ src/components/dashboard/dashboard.jsx
@@ -3,6 +3,78 @@ import { useRouter } from 'next/navigation';
 import React from 'react'
 import Image from 'next/image';
 
+import {
+  AreaChart,
+  Card,
+  Metric,
+  TabList,
+  Tab,
+  TabGroup,
+  TabPanels,
+  TabPanel,
+} from "@tremor/react";
+
+const data = [
+  {
+    Month: "Jan 17",
+    Spending: 75000,
+  },
+  //...
+  {
+    Month: "Jan 18",
+    Spending: 105000,
+  },
+  {
+    Month: "Jan 19",
+    Spending: 45000,
+  },
+  {
+    Month: "Jan 20",
+    Spending: 95000,
+  },
+  {
+    Month: "Jan 21",
+    Spending: 205000,
+  },
+  {
+    Month: "Jan 22",
+    Spending: 175000,
+  },
+  {
+    Month: "Jan 23",
+    Spending: 130000,
+  },
+  {
+    Month: "Jan 24",
+    Spending: 65000,
+  },
+  {
+    Month: "Jan 25",
+    Spending: 45000,
+  },
+  {
+    Month: "Jan 26",
+    Spending: 90000,
+  },
+  {
+    Month: "Jan 27",
+    Spending: 40000,
+  },
+];
+
+const numberFormatter = (value) => Intl.NumberFormat("us").format(value).toString();
+
+const percentageFormatter = (value) =>
+  `${Intl.NumberFormat("us")
+    .format(value * 100)
+    .toString()}%`;
+    
+function sumArray(array, metric) {
+  return array.reduce((accumulator, currentValue) => accumulator + currentValue[metric], 0);
+}
+
+import { DashboardHeader } from './dashboard.header';
+
 export const Dashboard = ({transactionData, limit}) => {
     const router = useRouter();
     let balance=0, i=0;
