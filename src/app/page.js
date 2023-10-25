b949634ea7ff76a8bb80acf08f8541e1e58dace7
Nuha Fadh <fayn.deco@gmail.com>
2023-10-26 05:04:04 +0700

    hero placeholder

--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,14 @@ import Image from 'next/image'
 
 import { AnnouncementBar } from '@/components/landingpage/announcementbar'
 import { NavBar } from '@/components/landingpage/navbar'
+import { HeroImage1 } from '@/components/landingpage/heroimage1'
 
 export default function Home() {
   return (
    <div>
     <AnnouncementBar />
     <NavBar />
+    <HeroImage1 />
    </div> 
   )
 }
