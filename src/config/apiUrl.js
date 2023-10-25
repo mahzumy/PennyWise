4ab5acf018df3743859939f985f173e57e067d41
mahzumy <mahzumy39@gmail.com>
2023-10-25 13:55:58 +0700

    deploy v.5

--- src/config/apiUrl.js
+++ src/config/apiUrl.js
@@ -1,3 +1,3 @@
 const DEV_API_URL="http://localhost:3000"
-const PROD_API_URL="http://penny-wise-six.vercel.app"
+const PROD_API_URL="https://penny-wise-six.vercel.app"
 export const API_URL = process.env.NODE_ENV === "development" ? `${DEV_API_URL}/api/v1` : `${PROD_API_URL}/api/v1`;
