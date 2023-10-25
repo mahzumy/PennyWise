845639863d591e6ee6c382f2296b0b52cb9dc55b
Nuha Fadh <fayn.deco@gmail.com>
2023-10-26 06:05:20 +0700

    hero image

--- src/components/landingpage/heroimage1.jsx
+++ src/components/landingpage/heroimage1.jsx
@@ -17,7 +17,7 @@ export const HeroImage1 = () => {
                     </a> 
                 </div>
                 <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
-                    <img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/phone-mockup.png" alt="mockup" />
+                    <img src="asset/mockup-hero.png" alt="mockup" />
                 </div>                
             </div>
             </section>
