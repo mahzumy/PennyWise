16079cafa24552802a3ab2c1f625c0d6a24f0910
Nuha Fadh <fayn.deco@gmail.com>
2023-10-26 23:18:42 +0700

    styling desktop

--- src/components/landingpage/videosection.jsx
+++ src/components/landingpage/videosection.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 export const VideoSection = () => {
   return (
     <div className='bg-blue-600 py-20'>
-        <div className='m bg-blue-600 max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 space-y-20'>
+        <div className=' bg-blue-600 max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 space-y-20'>
             <div className=' text-white tracking-normal'>
-                <h2 className=' text-5xl w-3/4'>Designed with the needs of individuals seeking fast transactions in mind.</h2>
+                <h2 className=' text-3xl md:text-4xl xl:text-5xl w-3/4'>Designed with the needs of individuals seeking fast transactions in mind.</h2>
             </div>
             <div>
                 <video
@@ -17,11 +17,13 @@ export const VideoSection = () => {
                     className='w-full cursor-pointer overflow-hidden border-white border-2 rounded-md '
                 />
             </div>
-            <div className='space-y-4'>
-                <h3 class="mb-4 sm:text-xl md:text-5xl lg:text-6xl font-extrabold text-gray-900 dark:text-white "><span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-400 from-blue-50">Spend, Record, Track</span></h3>
-                <p className='text-xl text-white w-3/4'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem expedita eligendi ipsa error adipisci, veniam id quasi iusto corporis earum nihil tenetur blanditiis laboriosam quibusdam molestias similique hic? Expedita, iste?</p>
+            <div className='space-y-10'>
+                <h3 class="mb-4 text-4xl md:text-5xl xl:text-6xl font-extrabold text-gray-900 dark:text-white "><span className='text-transparent bg-clip-text bg-gradient-to-r to-emerald-400 from-blue-50'>Spend, Record, Track</span></h3>
+                <p className='text-xl text-white w-3/4'>At Pennywise, we've made it incredibly simple for you to manage your finances. With our user-friendly features, you can effortlessly spend, easily record your expenses, and consistently track your financial journey. Take control of your money without the fuss."</p>
+                <div className=' text-emerald-400 font-semibold text-lg'>
+                    <a href="/register" className=' cursor-pointer pb-2 border-b-2 border-emerald-400'>Join Beta Product Now!</a>
+                </div>
             </div>
-            <div>Explore Pricing Plan</div>
         </div>
     </div>
   )
