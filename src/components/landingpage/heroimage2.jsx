1d2211ca331e7b5f167131716a4d4b587d3a7409
Nuha Fadh <fayn.deco@gmail.com>
2023-10-27 00:15:13 +0700

    fix landing page desktop + mobile not yet

--- src/components/landingpage/heroimage2.jsx
+++ src/components/landingpage/heroimage2.jsx
@@ -3,18 +3,21 @@ import React from 'react'
 export const HeroImage2 = () => {
   return (
     <div className=''>
-        <section className="bg-white dark:bg-gray-900">
-            <div className='grid max-w-screen-xl items-center mx-auto justify-center lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12'>
-                <div className='lg:mt-0 lg:col-span-5 lg:flex'>
-                    <img src="asset/pennywise-iphone-mockup.png" alt="mockup" className=' max-w-xs' />
+        <section className='bg-white dark:bg-gray-900'>
+            <div className='sm:inline md:grid max-w-screen-xl items-center mx-2 md:mx-auto justify-center lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12'>
+                <div className='lg:mt-0 sm:col-auto lg:col-span-5 lg:flex sm:px-5 md:mx-0 justify-center content-center'>
+                    <img src="asset/pennywise-iphone-mockup.png" alt="mockup" className='sm: w-52 md:max-w-xs' />
                 </div> 
-                <div className="mr-auto place-self-center lg:col-span-7">
-                    <h2 className="max-w-xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Everything you get with <span className='text-transparent bg-clip-text bg-gradient-to-r to-emerald-400 from-blue-600'>Pennywise</span></h2>
-                    <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">Cultivate financial wisdom with Pennywise: Track, save, and budget wisely – every penny counts on your path to prosperity!</p>
-                    <a href="#" className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
-                        Register Now
-                        <svg className="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                    </a>
+                <div className='mx-auto justify-center sm:inline-block lg:col-span-7'>
+                    <h2 className='max-w-xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white text-center md:text-left '>Everything you get with <span className='text-transparent bg-clip-text bg-gradient-to-r to-emerald-400 from-blue-600'>Pennywise</span></h2>
+                    <p className='max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400 text-center md:text-left'>Cultivate financial wisdom with Pennywise: Track, save, and budget wisely – every penny counts on your path to prosperity!</p>
+                    <div className='mx-2 justify-center'>
+                        <a href="#" className='inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900'>
+                            Register Now
+                            <svg className="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                        </a>
+                    </div>
+                    
                 </div>               
             </div>
             </section>
