aeaf56a0fc134a1103c111251529c758a6246090
Nuha Fadh <fayn.deco@gmail.com>
2023-10-27 00:15:13 +0700

    fix landing page desktop + mobile not yet

--- src/components/landingpage/minifeatures.jsx
+++ src/components/landingpage/minifeatures.jsx
@@ -3,21 +3,21 @@ import React from 'react'
 export const MiniFeatures = () => {
   return (
     <div className='py-20'>
-        <div className=' max-w-screen-xl px-4 py-8 lg:gap-8 xl:gap-0 lg:py-16 mx-auto'>
-            <div className=' flex col-span-3 w-full'>
-                <div className=' items-center px-10 pb-24 border-r border-b border-blue-600 w-full space-y-4'>
+        <div className=' flex max-w-screen-xl px-4 py-8 gap lg:gap-8 xl:gap-0 lg:py-16 mx-auto'>
+            <div className='sm:inline-block md:flex w-screen'>
+                <div className=' items-center px-10 pb-24 sm:border-none md:border-r md:border-b border-blue-600 w-fit space-y-4'>
                     <h3 className='text-center text-black text-3xl'>
                     Track your <span className='text-blue-600'>Expenses</span>
                     </h3>
                     <p className='text-center text-lg tracking-tight'>The tools you need for a clear view of your spending, so you're well-informed when making financial decisions</p>
                 </div>
-                <div className=' items-center px-10 pb-24 border-b border-blue-600 w-full space-y-4'>
+                <div className=' items-center px-10 pb-24 sm:border-none md:border-r md:border-b border-blue-600 w-dit space-y-4'>
                     <h3 className='text-center text-black0 text-3xl'>
                         Track your <span className='text-blue-600'>Income</span>
                     </h3>
                     <p className='text-center text-lg tracking-tight'>Monitoring your earnings becomes a breeze, giving you a clear picture of your financial inflow and empowering smarter money management</p>
                 </div>
-                <div className=' items-center px-10 pb-24 border-l border-b border-blue-600 w-full space-y-4'>
+                <div className=' items-center px-10 pb-24 sm:border-none md:border-r md:border-b border-blue-600 w-fit space-y-4'>
                     <h3 className=' text-center text-black text-3xl'>
                         Track your <span className='text-blue-600'>Habit</span>
                     </h3>
--- src/components/landingpage/navbar.jsx
+++ src/components/landingpage/navbar.jsx
@@ -11,7 +11,7 @@ export const NavBar = () => {
                 </div>
                 <div class="flex space-x-2 md:order-2">
                     <a className='invisible md:visible sm:w-0 md:w-fit' href="/login">
-                        <button type="button" className='text-blue-600 hover:text-white border border-blue-600 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-md px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'>Login</button>
+                        <button type="button" className='text-blue-600 hover:text-white border border-blue-600 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'>Login</button>
                     </a>
                     <a href="/register">
                         <button type="button" className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'>Get started</button>
