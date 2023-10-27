c3affefe1294020ed3f3a24ddfbacab106469162
Nuha Fadh <fayn.deco@gmail.com>
2023-10-27 15:13:10 +0700

    fix padding on detail and add transaction

--- src/components/dashboard/transaction/transaction.card.jsx
+++ src/components/dashboard/transaction/transaction.card.jsx
@@ -8,7 +8,7 @@ export const TransactionCard = () => {
   const router = useRouter();
 
   return (
-    <div className='w-full p-6 bg-white dark:bg-gray-800 dark:border-gray-700 mb-4 space-y-5'>
+    <div className='w-full p-6 bg-white dark:bg-gray-800 dark:border-gray-700 mb-4 space-y-5 px-5'>
           <div className='flex space-x-1 cursor-pointer' onClick={()=>router.push(`/dashboard`)}>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-7 h-7 stroke-blue-600">
               <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
