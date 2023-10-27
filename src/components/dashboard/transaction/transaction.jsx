f5a1ba70af5b9861b75badb6e75e5a126325eed3
Nuha Fadh <fayn.deco@gmail.com>
2023-10-27 15:05:35 +0700

    fix add transaction layout

--- src/components/dashboard/transaction/transaction.jsx
+++ src/components/dashboard/transaction/transaction.jsx
@@ -6,7 +6,7 @@ import { DashboardHeader } from '@/components/dashboard/dashboard.header'
 
 export const Transaction = () => {
   return (
-    <div className='w-[450px] justify-center items-center m-auto h-screen space-y-2 mb-4'>
+    <div className='w-[450px] justify-center items-center m-auto h-screen mb-4'>
         <DashboardHeader />
         <TransactionCard />
     </div>
