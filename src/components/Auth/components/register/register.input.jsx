b1e10b8ba44c4951f38725c5973670d85b4ce9e5
Nuha Fadh <fayn.deco@gmail.com>
2023-10-28 09:10:20 +0700

    add login link to register page

--- src/components/Auth/components/register/register.input.jsx
+++ src/components/Auth/components/register/register.input.jsx
@@ -27,6 +27,10 @@ export const RegisterInput = () => {
               </div>          
             </div>
             <button className='w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800' onClick={handleSubmitRegister}>Register</button>
+            <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+              Already have an account? <a href="/login" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Login here</a>
+            </p>
+            
         </div>
   )
 }
