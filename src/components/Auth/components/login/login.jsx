d36b76279f1eac5fb8ce833e82357442fe6c4866
nuhafadh <144405123+nuhafadh@users.noreply.github.com>
2023-10-28 18:47:22 +0700

    Update login.jsx
    add home link

--- src/components/Auth/components/login/login.jsx
+++ src/components/Auth/components/login/login.jsx
@@ -9,7 +9,7 @@ export const Login = () => {
 
   return (
     <div className='flex flex-col items-center justify-center px-6 py-8 mx-auto space-y-5 md:h-screen lg:py-0'>
-      <a href="#" className='flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white'>
+      <a href="/" className='flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white'>
         <img src="/asset/pennywise-dummy-logo.png" width={240} alt="logo-pennywise" />
       </a>
       <LoginCard />
